41bf17226afb78d9cf759a66783f1e331b5cba2a
Izabella Ribeiro <bellarpjf@gmail.com>
2020-10-18 23:06:08 -0300

    Exercicio de repeticao

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Primeiro from './components/Primeiro';
 import ComParametro from './components/ComParmetro';
 import Aleatorio from './components/Aleatorio';
 import Card from './components/layout/Card';
+import Familia from './components/Familia';
+import FamiliaMembro from './components/FamiliaMembro';
+import ListaAlunos from './components/repeticao/ListaAlunos';
 
 function App() {
   return (
@@ -12,6 +15,17 @@ function App() {
       <div className="App-content">
           Mini Projetos
           <div className="cards">
+            <Card titulo="Repetição" color="#161a1d">
+              <ListaAlunos sobrenome="Ribeiro">
+              </ListaAlunos>
+            </Card>
+            <Card titulo="Membros da família" color="#7400b8">
+              <Familia sobrenome="Ribeiro">
+                <FamiliaMembro nome="Maria" />
+                <FamiliaMembro nome="José" />
+                <FamiliaMembro nome="Camila" />
+              </Familia>
+            </Card>
             <Card titulo="Número aleatório" color="#003049">
               <Aleatorio primeiroNumero={1} 
               segundoNumero={10}/>
---
+++ src/data/alunos.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, nome: 'João', nota: '8.9'},
+    { id: 2, nome: 'Vanesa', nota: '8.9'},
+    { id: 3, nome: 'Renata', nota: '8.9'},
+    { id: 4, nome: 'Felipe', nota: '8.9'},
+    { id: 5, nome: 'Mateus', nota: '8.9'}
+]
